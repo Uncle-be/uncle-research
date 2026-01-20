4c81538fbecdc89ebcd080ca6425469a6232cfa3
Blessing Rapheal <115481113+Uncle-be@users.noreply.github.com>
2026-01-20 06:38:00 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,21 +1,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
-const mountNode = document.getElementById('root');
-
-if (mountNode) {
-  const root = ReactDOM.createRoot(mountNode);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-  
-  // Explicitly hide the loader once rendering starts
-  const loader = document.getElementById('initial-loader');
-  if (loader) {
-    loader.style.display = 'none';
-  }
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
 }
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
